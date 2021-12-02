d9791ab439dbe1311fadd750e3f4a9fa497e4a49
Alexander Mann <amann@c1r4p5.hive.fi>
2021-12-02 17:44:04 +0200

    working prototype done, still a bit messy

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,28 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   brackets.h                                         :+:      :+:    :+:   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amann <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/01 15:37:56 by amann             #+#    #+#             */
-/*   Updated: 2021/12/02 11:12:52 by amann            ###   ########.fr       */
+/*   Created: 2021/12/01 15:50:03 by amann             #+#    #+#             */
+/*   Updated: 2021/12/01 15:51:59 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BRACKETS_H
-# define BRACKETS_H
+#include "brackets.h"
 
-# include <stdio.h>
-# include <unistd.h>
-
-typedef struct s_list
+void	ft_putchar(char c)
 {
-	char			b;
-	struct s_list 	*next;
-}					t_list;
+	write(1, &c, 1);
+}
 
-void	ft_putchar(char c);
-void	ft_putstr(char *s);
+void	ft_putstr(char *s)
+{
+	int i;
 
-#endif
+	i = 0;
+	while (s[i] != '\0')
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+	
+}
--- main.c
+++ main.c
@@ -6,15 +6,125 @@
 /*   By: amann <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 15:35:12 by amann             #+#    #+#             */
-/*   Updated: 2021/12/01 15:36:57 by amann            ###   ########.fr       */
+/*   Updated: 2021/12/02 17:42:53 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "brackets.h"
 
+//should handle multiple strings, output newline if no args
+//if good: OK\n if bad: Error\n
+//Brackets, square brackets and braces should be considered brackets
+//A string with no brackets is ok
+
+int		check_and_update_previous(char *s, char a, int i)
+{
+	while (i >= 0)
+	{	
+		if (a == ')')
+		{
+			if (s[i] == '(')
+			{
+				s[i] = 'a';
+				return (1);
+			}
+			else if (s[i] == '{' || s[i] == '[')
+				return (0);
+		}
+		else if (a == ']')
+		{
+			if (s[i] == '[')
+			{
+				s[i] = 'a';
+				return (1);
+			}
+			else if (s[i] == '(' || s[i] == '{')
+				return (0);
+		}
+		else if (a == '}')
+		{
+			if (s[i] == '{')
+			{	
+				s[i] = 'a';
+				return (1);
+			}
+			else if (s[i] == '(' || s[i] == '[')
+				return (0);
+		}
+		i--;
+	}
+	return (0);
+}
+
+int		ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	   i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int		checkbrackets(char *str)
+{
+	int i;
+	char a;
+ 	char s[ft_strlen(str)];
+	
+	ft_strcpy(s, str);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		a = s[i];
+		if (a == ')' || a == ']' || a == '}')
+		{
+			if (!check_and_update_previous(s, a, i))
+				return (0);
+		}
+		i++;
+	}
+	i = 0;
+	while (s[i] != '\0')
+	{
+		a = s[i];
+		if (a == '(' || a == '[' || a == '{')
+			return (0);
+		i++;
+	}
+	return (1);
+}	
+
+void	checkbrackets_control(char **argv)
+{
+	int i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (checkbrackets(argv[i]))
+			ft_putstr("OK\n");
+		else
+			ft_putstr("Error\n");
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	
-
+	if (argc != 1)
+		checkbrackets_control(argv + 1);
+	else
+		ft_putchar('\n');
 	return (0);
 }
